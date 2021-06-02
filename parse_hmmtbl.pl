806aeedc781e5762b60d2cf6802d7b6a1b56b256
Pombert-JF <jpombert@iit.edu>
2021-06-02 17:50:45 -0500

    Fixed regex for non-sprot databases

--- parse_hmmtbl.pl
+++ parse_hmmtbl.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 ## Pombert Lab, 2018
 my $name = 'parse_hmmtbl.pl';
-my $version = '0.2b';
-my $updated = '2021-04-06';
+my $version = '0.3';
+my $updated = '2021-06-02';
 
 use strict; use warnings; use Getopt::Long qw (GetOptions);
 
@@ -32,23 +32,52 @@ GetOptions(
 
 ## Print table header
 open OUT, ">", "$table" or die "Can't create file $table: $!\n";
-print OUT "Query\tTarget\tE-value\tProduct\tGenus\tSpecies\tOS descriptor\n";
+my $hflag = 0;
 
 ## Parsing files
 while (my $file = shift@tbl){
 	open IN, "<", "$file" or die "Can't read file $file: $!\n";
 		while (my $line = <IN>){
 		chomp $line;
-		if ($line =~ /^#/){ next;} ## Skipping comments 
-		## File format is space separated, must use a regex...
-		elsif ($line =~ /^(\S+)\s+-\s+(\S+)\s+-\s+(\S+e-\d+)(?:\s+\S+){6}\s+(?:\d+\s+){7}(.*)\sOS=(.*?)(?:OX|GN|PE|SV)=/){
-			my $target = $1;
-			my $query = $2;
-			my $evalue = $3;
-			my $product = $4;
-			my $OS = $5;
-			my ($species,$genus) = $OS =~ /^((\S+)\s+\S+)/;
-			print OUT "$query\t$target\t$evalue\t$product\t$genus\t$species\t$OS\n";
+		if ($line =~ /^#/){ next; } ## Skipping comments 
+		else {
+			## File format is space separated, must use a regex...
+			my @columns = split (/\s+/, $line);
+		
+			my $target = $columns[0];
+			my $taccession = $columns[1];
+			my $query = $columns[2];
+			my $qaccession = $columns[3];
+			my $fevalue = $columns[4];
+			my $fscore = $columns[5];
+			my $fbias = $columns[6];
+			my $devalue = $columns[7];
+			my $dscore = $columns[8];
+			my $dbias = $columns[9];
+			## 10-17 domain number estimation
+
+			my $description;
+			for my $num (18..$#columns){
+				$description .= "$columns[$num] ";
+			}
+
+			if ($description =~ /(.*)\sOS=(.*?)(?:OX|GN|PE|SV)=/){
+				my $product = $1;
+				my $OS = $2;
+				my ($species,$genus) = $OS =~ /^((\S+)\s+\S+)/;
+				if ($hflag == 0){
+					print OUT "Query\tTarget\tE-value\tProduct\tGenus\tSpecies\tOS descriptor\n";
+					$hflag = 1;
+				}
+				print OUT "$query\t$target\t$fevalue\t$product\t$genus\t$species\t$OS\n";
+			}
+			else {
+				if ($hflag == 0){
+					print OUT "Query\tTarget\tE-value\tDescription\n";
+					$hflag = 1;
+				}
+				print OUT "$query\t$target\t$fevalue\t$description\n";
+			}
 		}
 	}
 }
